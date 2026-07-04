a93c4d5c1759c02733d49d23a4d081e352b8eb9f
AnasZayed132 <trainhiit007@gmail.com>
2026-07-04 13:03:12 +0200

    Add files via upload

---
+++ hw2.py.py
@@ -0,0 +1,30 @@
+int=input("what is your name friend 1")
+
+print("hello ",int)
+age=input("what is your date of birth")
+print("best of luck",int)
+
+frnd=input("what is your name friend 2")
+
+print("hello ",frnd)
+age1=input("what is your date of birth")
+print("best of luck",frnd)
+
+age3=input("what is your name friend 3")
+
+print("hello ",age3)
+age1=input("what is your date of birth")
+print("best of luck",age3)
+
+age14=input("what is your name friend 4")
+
+print("hello ",age14)
+age4=input("what is your date of birth")
+print("best of luck",age14)
+
+
+age5=input("what is your name friend 5")
+
+print("hello ",age5)
+age51=input("what is your date of birth")
+print("best of luck",age5)
